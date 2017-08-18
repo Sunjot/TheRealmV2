c48967a516448c67f5c0e72573f6d2c770315644
Sunjot Singh <sunjot@uoguelph.ca>
2017-08-18 02:58:40 -0400

    worked on world component

--- src/Components/Intro.js
+++ src/Components/Intro.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import Header from './Pages/Header';
 import Start from './Pages/Intro/Start';
-import Westeros from './Pages/Intro/Westeros';
+import World from './Pages/Intro/World';
 import Structure from './Pages/Intro/Structure';
 import Houses from './Pages/Intro/Houses';
 import Rebellion from './Pages/Intro/Rebellion';
@@ -14,7 +14,7 @@ class Intro extends Component {
     this.handleClick = this.handleClick.bind(this);
 
     this.state = {
-      sectionClicked: <Start />
+      sectionClicked: <World />
     };
   }
 
@@ -22,7 +22,7 @@ class Intro extends Component {
 
     let sec = null; // set component based on which table item was clicked
     if (res === "0") sec = <Start />;
-    if (res === "1") sec = <Westeros />;
+    if (res === "1") sec = <World />;
     if (res === "2") sec = <Structure />;
     if (res === "3") sec = <Houses />;
     if (res === "4") sec = <Rebellion />;
@@ -35,7 +35,7 @@ class Intro extends Component {
   render() {
     let tableItems = [
       {name: "Starting out", id: "0"},
-      {name: "Westeros", id: "1"},
+      {name: "The World", id: "1"},
       {name: "Social structure", id: "2"},
       {name: "Houses", id: "3"},
       {name: "Robert's Rebellion", id: "4"}
