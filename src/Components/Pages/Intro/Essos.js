1b6c7cc522cd73fc4634a584686e983e017b4fdd
Sunjot Singh <sunjot@uoguelph.ca>
2017-12-18 00:57:14 -0500

    finished content for essos page

--- src/Components/Pages/Intro/Essos.js
+++ src/Components/Pages/Intro/Essos.js
@@ -27,22 +27,35 @@ class Essos extends Component {
               <br/><br/>
               The surviving colonies from this disaster became known as what is now the Free Cities.
               These city-states include: Braavos, Lorath, Lys, Myr, Norvos, Pentos, Qohor, Tyroth and Volantis.
-              Braavos (pictured here) is the most influential of these city-states and is also the location for the Iron Bank of 
-              Braavos - the most powerful bank in the Known World, as it funds the Iron Throne.</p>
+              Braavos (pictured here) is the most influential of these city-states and is also the location for
+              the Iron Bank of Braavos - the most powerful bank in the Known World, as it funds the Iron Throne.</p>
             </div>
           </div>
           <div id="slavers-cont" className="essos-subsec">
             <p className="miniheading">Slaver's Bay</p>
             <div className="desc-img">
               <img id="" src={Meereen}></img>
-              <p className="desc"></p>
+              <p className="desc">Okay, I lied just slightly. Some of the surviving colonies of the volcanic
+              eruptions also turned into Slaver's Bay, which lies southeast of the Free Cities. There are 3 city-states
+              in Slaver's Bay that are considered the most powerful and so we only need to worry about those: Meereen,
+              Astapor, and Yunkai.
+              <br/><br/>As the name suggests, Slaver's Bay is rampant with slavery. Well technically,
+              majority of Essos is (including some of the Free Cities - Braavos being one of the exceptions).
+              However, that name is given to this particular region because it is the location for the international
+              slave trade.</p>
             </div>
           </div>
           <div id="dothraki-cont" className="essos-subsec">
             <p className="miniheading">Dothraki Sea</p>
             <div className="desc-img">
               <img id="" src={Dothraki}></img>
-              <p className="desc"></p>
+              <p className="desc">It's not what you think it is. The "sea" refers to a sea of
+              grass east of the Free Cities, not an actual sea. This area is run by these terrifying looking
+              warriors called Dothraki's (shocker) who are mounted on horses. They don't believe in money and
+              so they frequently raid other areas in search for goods. They almost solely fight while on their
+              horses (they can fire bows on top of using swords/whips). Have something they want? Let them
+              have it or fight to the death for it. If you've seen what these guys look like, I'd go the prior.
+              </p>
             </div>
           </div>
         </div>
