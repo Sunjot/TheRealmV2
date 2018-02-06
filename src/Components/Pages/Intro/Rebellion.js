a293ffb452b2bb57acc0900c66012185372b927c
Sunjot Singh <sunjot@uoguelph.ca>
2018-02-06 02:49:32 -0500

    worked on mobile capab for houses comp

--- src/Components/Pages/Intro/Rebellion.js
+++ src/Components/Pages/Intro/Rebellion.js
@@ -6,6 +6,13 @@ class Rebellion extends Component {
     return (
       <div className="content">
         <p className="subheading">Robert's Rebellion</p>
+        <p className="desc-cont">If you've read any of the previous sections, you probably saw mentions of
+          Robert's Rebellion. Well - turns out - it is one of the biggest series of events in the Game of Thrones
+          Universe and essential knowledge before you dive into the show. It occurs not long before beginning
+          of the series and sets up the current state in Westeros. You could probably read into it a lot more
+          elsewhere, but I will try to summarize the events the best I can.
+        </p>
+        
       </div>
     );
   }
