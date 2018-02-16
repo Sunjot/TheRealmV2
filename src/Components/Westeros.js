a205ca30484c2b47e1f37dfaaa82f6d6a4dbe20d
Sunjot Singh <sunjot@uoguelph.ca>
2018-02-16 05:53:01 -0500

    modif to westeros design done for web/mobile

--- src/Components/Westeros.js
+++ src/Components/Westeros.js
@@ -11,15 +11,16 @@ class Westeros extends Component {
       <div>
         <Header/>
         <div className="content">
-          <p className="subheading">Westeros</p>
-          <p className="desc-cont">If you read the previous section, you know that I mentioned the importance
-            of Westeros. Almost all the action in the show takes place here. I also made a point to mention
-            that this continent is split into two areas - the Seven Kingdoms and the area Beyond the Wall.
-            However, it isn't that simple. We must break it down even further! I mean, it's called the Seven
-            Kingdoms for a reason - it's important we go into detail on what these kingdoms actually are.
-            Not only that, but what exactly lies Beyond the Wall? Not to worry, everything will be addressed!
-          </p>
-          <div id="westeros-divider"></div>
+          <div id="westeros-info-cont">
+            <p className="subheading">Westeros</p>
+            <p className="desc-cont">If you read the previous section, you know that I mentioned the importance
+              of Westeros. Almost all the action in the show takes place here. I also made a point to mention
+              that this continent is split into two areas - the Seven Kingdoms and the area Beyond the Wall.
+              However, it isn't that simple. We must break it down even further! I mean, it's called the Seven
+              Kingdoms for a reason - it's important we go into detail on what these kingdoms actually are.
+              Not only that, but what exactly lies Beyond the Wall? Not to worry, everything will be addressed!
+            </p>
+          </div>
           <div id="westeros-cont">
             <div id="seven-kingdoms" className="westeros-subsec">
               <div className="info">
