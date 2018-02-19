40e613ebbe04cdc542e2a7da36106057782add62
Sunjot Singh <sunjot@uoguelph.ca>
2018-02-19 06:21:24 -0500

    worked on slideout menu on mobile

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,20 +1,36 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
+import * as Icon from 'react-feather';
 
 class Header extends Component {
 
   constructor() {
     super();
     this.checkResize = this.checkResize.bind(this);
+    this.navExpand = this.navExpand.bind(this);
+    this.navCollapse = this.navCollapse.bind(this);
 
     this.state = {
-      winW: window.innerWidth
+      winW: window.innerWidth,
+      navToggle: ''
     };
   }
 
   checkResize(e) {
     this.setState({
       winW: window.innerWidth
+    });
+  }
+
+  navExpand() {
+    this.setState({
+      navToggle: 'expand-nav'
+    });
+  }
+
+  navCollapse() {
+    this.setState({
+      navToggle: 'collapse-nav'
     });
   }
 
@@ -40,14 +56,34 @@ class Header extends Component {
       <div id="headerCont">
         <Link to="/"><div id="home-left" ></div></Link>
         <div id="headerMiddle">
-          { links.map(l => {
-            return <Link
-                      className="header-links"
-                      to={l.link}
-                      key={l.name}>
-                        {this.state.winW > 650? l.name : l.short}
-                    </Link>
-          })}
+          { this.state.winW > 650 &&
+            links.map(l => {
+              return <Link
+                        className="header-links"
+                        to={l.link}
+                        key={l.name}>
+                          {l.name}
+                      </Link>
+            })
+          }
+          { this.state.winW < 650 &&
+            <div id="mobile-cont">
+              <div id="headerIcon">
+                <Icon.Menu color="black" size={34} onClick={() => this.navExpand()} />
+              </div>
+              <div id="mobile-nav" className={this.state.navToggle}>
+                <Icon.X color="black" id="close-icon" size={24} onClick={() => this.navCollapse()} />
+                { links.map(l => {
+                    return <Link
+                              className="header-links-m"
+                              to={l.link}
+                              key={l.name}>
+                                {l.name}
+                            </Link>
+                })}
+              </div>
+            </div>
+          }
         </div>
       </div>
     );
